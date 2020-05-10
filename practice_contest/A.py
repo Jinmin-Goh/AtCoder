65497a3c63fc6e4224e6a854d803142d596d8498
Jinmin Goh <eric970901@gmail.com>
2020-05-11 01:17:48 +0900

    practice contest - problem A solution, accepted

---
+++ practice_contest/A.py
@@ -0,0 +1,16 @@
+# Contest No.: practice
+# Problem No.: A
+# Solver:      JEMINI
+# Date:        20200508
+
+import sys
+
+def main():
+    a = int(input())
+    b, c = map(int, input().split())
+    s = input()
+    print(a + b + c, s)
+    return
+
+if __name__ == "__main__":
+    main()
