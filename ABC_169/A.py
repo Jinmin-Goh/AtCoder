2af0a582d6f2ca9bcd95cc0375df48e2c37d0e5f
Jinmin Goh <eric970901@gmail.com>
2020-05-31 23:18:17 +0900

    ABC 169 - problem A solution, accepted

---
+++ ABC_169/A.py
@@ -0,0 +1,15 @@
+# Contest No.: ABC169
+# Problem No.: A
+# Solver:      JEMINI
+# Date:        20200531
+
+import sys
+import heapq
+
+def main():
+    a, b = map(int, sys.stdin.readline().split())
+    print(a * b)
+    return
+
+if __name__ == "__main__":
+    main()
