e1d1e636cf5b9e24ad18a1739aec326bcd327dc5
Jinmin Goh <eric970901@gmail.com>
2020-06-15 03:00:28 +0900

    ABC 170 - problem A solution, AC

---
+++ ABC_170/A.py
@@ -0,0 +1,17 @@
+# Contest No.: ABC170
+# Problem No.: A
+# Solver:      JEMINI
+# Date:        20200614
+
+import sys
+import heapq
+
+def main():
+    nums = list(map(int, input().split()))
+    print(nums.index(0) + 1)
+
+
+    return
+
+if __name__ == "__main__":
+    main()
