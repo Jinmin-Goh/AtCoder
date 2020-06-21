fbcb8c6ab532263d9d4d69145cc467ae13a9d2bc
Jinmin Goh <eric970901@gmail.com>
2020-06-22 03:54:16 +0900

    ABC 171 - problem A solution, AC

---
+++ ABC_171/A.py
@@ -0,0 +1,18 @@
+# Contest No.: ABC171
+# Problem No.: A
+# Solver:      JEMINI
+# Date:        20200621
+
+import sys
+import heapq
+
+def main():
+    n = input()
+    if n in "abcdefghijklmnopqrstuvwxyz":
+        print("a")
+    else:
+        print("A")
+    return
+
+if __name__ == "__main__":
+    main()
